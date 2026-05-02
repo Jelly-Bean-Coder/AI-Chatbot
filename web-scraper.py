526b49a57e77f21acd8d42020877872d81b2a160
Jordan <im0947362@gmail.com>
2026-05-01 18:54:19 -0600

    Scrape output to text file

--- web-scraper.py
+++ web-scraper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 links = []
-text = ""
 
 headers = {
     'User-Agent': 'MyCoolScraper/1.0 (im0947362@gmail.com)'
@@ -11,13 +10,20 @@ headers = {
 with open("websites.txt", "r") as file:
     for line in file.readlines():
         links.append(line.strip())
-        print(line)
 
 for link in links:
     response = requests.get(link, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
+    first = True
+    with open("info.txt", "w", encoding="utf-8") as file:
+        if first:
+            first = False
+            file.write("==ARTICLE-START==\n\n")
 
-    text += "\n\n" + str(soup.get_text())
-    text = "\n".join(line for line in text.splitlines() if line.strip())
+        else:
+            file.write("\n==ARTICLE-START==\n\n")
 
-print(text)
+        text = str(soup.get_text())
+        text = "\n".join(line for line in text.splitlines() if line.strip())
+        file.write(text)
+        file.write("\n\n==ARTICLE-END==\n")
