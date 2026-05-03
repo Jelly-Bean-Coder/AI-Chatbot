901e91fd81e1c4fb0ea9119c65247feb5cfc76d7
Jordan <im0947362@gmail.com>
2026-05-03 15:07:31 -0600

    Matrix
    Finished making the matrix and implementing save logic to brain.npy file

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,18 @@
 import nltk
 import typing
+import numpy as np
+import datetime
+import json
+
 
 nltk.download("punkt_tab")
 
-separated_sources = []
 start_token = "==ARTICLE-START=="
 end_token = "==ARTICLE-END=="
 
 
 def separate_sources(file: typing.IO):
+    separated_sources = []
     with open(file, "r", encoding="utf-8") as file:
         current_string = ""
         text = file.readlines()
@@ -28,17 +32,58 @@ def separate_sources(file: typing.IO):
             if appending and new_item != start_token:
                 current_string += " " + str(item.strip())
 
+    return separated_sources
+
 def tokenize(text):
     stemmer = nltk.stem.snowball.SnowballStemmer("english")
-    stemmed = []
+    grand_stemmed = []
     for source in text:
+        article_stemmed = []
         words = nltk.word_tokenize(source)
         for word in words:
-            stemmed.append(stemmer.stem(word))
-    print(stemmed)
+            article_stemmed.append(stemmer.stem(word))
 
-def main(file_path: str):
-    separate_sources(file_path)
-    tokenize(separated_sources)
+        grand_stemmed.append(article_stemmed)
 
-main("info.txt")
+    return grand_stemmed
+
+def flatten(list):
+    flattened = []
+    for item in list:
+        if isinstance(item, list):
+            flattened.extend(flatten(item))
+
+        else:
+            flattened.append(item)
+
+    return flattened
+
+def create_matrix():
+    separated_sources = separate_sources(file="info.txt")
+    tokens = tokenize(separated_sources)
+
+    map = sorted(set(flatten(tokens)))
+    data = np.zeros((len(separated_sources), len(map)), dtype="float32")
+
+    for article, article_content in enumerate(tokens):
+        for word in article_content:
+            if word in map:
+                data[article, map.index(word)] = 1.0
+
+    return (map, data, separated_sources)
+
+
+
+def update_files(map, data, separated_sources):
+    knowlege = {
+        "map": map,
+        "last-updated": datetime.datetime.now().date().isoformat(),
+        "article-count": len(separated_sources)
+    }
+    with open("knowledge.json", "w", encoding="utf-8") as file:
+        json.dump(knowlege, file)
+
+    np.save("brain.npy", data)
+
+
+
