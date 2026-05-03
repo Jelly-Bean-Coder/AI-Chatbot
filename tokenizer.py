7ba67d4f998e1476e45ac698c6ec1509e4afc2ef
Jordan <im0947362@gmail.com>
2026-05-03 15:07:31 -0600

    Matrix
    Finished making the matrix and implementing save logic to brain.npy file

--- tokenizer.py
+++ tokenizer.py
@@ -1,14 +1,15 @@
 import nltk
 import typing
+import numpy as np
 
 nltk.download("punkt_tab")
 
-separated_sources = []
 start_token = "==ARTICLE-START=="
 end_token = "==ARTICLE-END=="
 
 
 def separate_sources(file: typing.IO):
+    separated_sources = []
     with open(file, "r", encoding="utf-8") as file:
         current_string = ""
         text = file.readlines()
@@ -28,17 +29,44 @@ def separate_sources(file: typing.IO):
             if appending and new_item != start_token:
                 current_string += " " + str(item.strip())
 
+    return separated_sources
+
 def tokenize(text):
     stemmer = nltk.stem.snowball.SnowballStemmer("english")
-    stemmed = []
+    grand_stemmed = []
     for source in text:
+        article_stemmed = []
         words = nltk.word_tokenize(source)
         for word in words:
-            stemmed.append(stemmer.stem(word))
-    print(stemmed)
+            article_stemmed.append(stemmer.stem(word))
 
-def main(file_path: str):
-    separate_sources(file_path)
-    tokenize(separated_sources)
+        grand_stemmed.append(article_stemmed)
 
-main("info.txt")
+    return grand_stemmed
+
+def flatten(list):
+    flattened = []
+    for item in list:
+        if isinstance(item, list):
+            flattened.extend(flatten(item))
+
+        else:
+            flattened.append(item)
+
+    return flattened
+
+separated_sources = separate_sources(file="info.txt")
+tokens = list(dict.fromkeys(tokenize(separated_sources)))
+
+map = sorted(set(flatten(tokens)))
+data = np.zeros((len(separated_sources), len(map)), dtype="float32")
+
+for article, article_content in enumerate(tokens):
+    for word in article_content:
+        if word in map:
+            data[article, map.index(word)] = 1.0
+
+np.save("brain.npy", data)
+
+
+
